adfc465fe48da09ff3d42fa822e69c8f8d671e60
Tom Morris <thomasmorris823@gmail.com>
2023-12-07 19:34:09 +0000

    Add bad files for tflint test

--- terraform/foo.tf
+++ terraform/foo.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "web" {
    
   #checkov:skip=CKV_AWS_79:Metadata of EC2 Instance not required
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.allow_sg.id]
-  iam_instance_profile   = var.iam_instance_profile
+  ami                    = "ami-0573ef119dcb77219"
+  instance_type          = "t2.micrro"
+  key_name               = "demo"
+  iam_instance_profile   = "demo-profile"
   
   #tfsec:ignore:aws-ec2-enforce-http-token-imds 
   metadata_options {
